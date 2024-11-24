6d76376b8a555ed24924a69c4c95a13e737e0a4e
Roy-Sebastian <roysurbakti23@gmail.com>
2024-11-24 17:26:51 +0700

    tes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // For static site generation
+  distDir: 'build', // Specify build directory
+  images: {
+    unoptimized: true // Required for static export
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
